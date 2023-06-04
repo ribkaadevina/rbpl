6ba7a95a2c79676f5c05119270ceee9d7b43daf0
ribkaadevina <117441160+ribkaadevina@users.noreply.github.com>
2023-06-04 21:15:08 +0700

    Add files via upload

---
+++ history.blade.php
@@ -0,0 +1,67 @@
+@extends('templatehome')
+@section('isi')
+@section('sidebar')
+    <nav class="sidenav nav nav-pills flex-column ml-4">
+        <a class="nav-link mb-2" aria-current="page" href="#">
+            <i class="fa-solid fa-list-check mr-3"></i>
+            My Task</a>
+        <a class="nav-link mb-2" href="#">
+            <i class="fa-solid fa-file-lines mr-3"></i>
+            Workspace</a>
+        <a class="nav-link mb-2" href="#">
+            <i class="fa-solid fa-users mr-3"></i>
+            Forum</a>
+        <a class="choose nav-link mb-2">
+            <i class="fa-regular fa-square-check mr-3"></i>
+            History</a>
+        <a class="nav-link mb-2">
+        </a>
+        <a class="nav-link mb-2">
+            <i class="fa-solid fa-arrow-right-from-bracket mr-3"></i>
+            Log Out</a>
+    </nav>
+@endsection
+
+@section('isi')
+<div class="done col-md-1 mt-4 ml-4" style="background-color: #8FC6FD; border-radius:5px;">
+    <p style="font-weight:800">
+        Done
+    </p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Latar Belakang PKM <i class="fa-solid fa-caret-right mr-3 mt-1" style="float: right; color:#8FC6FD;"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Dynaform ProcessMaker <i class="fa-solid fa-caret-right mr-3 mt-1" style="float: right; color:#8FC6FD;"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Use Case APERTA <i class="fa-solid fa-caret-right mr-3 mt-1" style="float: right; color:#8FC6FD;"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+<div class="taskdone col-md-11 ml-4" style="background-color: white; border-radius:30px;">
+    <p class="pt-3 pb-3"> Belakang PKM <i class="fa-solid fa-caret-right" style="float: right"></i></p>
+</div>
+
+
+</div>
+@endsection
